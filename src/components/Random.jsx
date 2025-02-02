3422d9042ab2f84836f3948210e9a65b3c9f8589
sharmabhi0911 <sharmabhishek0911@gmail.com>
2025-02-01 23:37:26 -1200

    first commit

---
+++ src/components/Random.jsx
@@ -0,0 +1,51 @@
+import useGif from "../hooks/useGif";
+import Spinner from "./Spinner";
+import React from "react";
+
+const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+
+const Random = () => {
+  // const [gif, setGif] = useState("");
+  // const [loading, setLoading] = useState(false);
+
+  // async function fetchData() {
+  //   setLoading(true);
+  //   const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+
+  //   const { data } = await axios.get(url);
+  //   const imageSource = data.data.images.downsized_large.url;
+  //   setGif(imageSource);
+  //   // console.log(imageSource);
+  //   setLoading(false);
+  // }
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+
+  const { gif, loading, fetchData } = useGif();
+
+  return (
+    <div className="w-1/2 bg-green-500 mx-auto rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
+      
+      <h1 className="mt-[15px] text-3xl uppercase underline font-bold">
+        A Random Gif
+      </h1>
+
+
+      {loading ? <Spinner /> : <img src={gif} width="450" />}
+
+
+      <button
+        onClick={() => fetchData()}
+        className="w-10/12 mb-[20px] bg-white text-xl py-2 rounded-lg font-bold"
+      >
+        Generate
+      </button>
+
+    </div>
+  );
+};
+
+export default Random;
